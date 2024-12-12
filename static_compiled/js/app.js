8f672ec7afb910a9a9f2abd815f0a2bea4b806a0
Nick Moreton <nickmoreton@me.com>
2024-12-12 17:38:01 +0000

    Don't ignore static_compiled/ directory
    Staic file are required to be used on python anywhere
    
    Thye can't be built on python anywhere

---
+++ static_compiled/js/app.js
@@ -0,0 +1,2 @@
+(()=>{var e=class{constructor(s=3){this.message="Hello from Wagtail starter kit!",this.times=s}showMessage(){for(let s=0;s<this.times;s++)console.log(this.message)}},t=new e;t.showMessage();})();
+//# sourceMappingURL=app.js.map
